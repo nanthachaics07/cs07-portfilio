a05e9d8e8b85c03edca8bf3d72e8e6be0eda733d
Nanthachai.CS07 <botsafe002@gmail.com>
2024-01-03 11:33:28 +0700

    releast v1.0.0

--- Script/script_port_web.js
+++ Script/script_port_web.js
@@ -36,7 +36,9 @@ class StickyNavigation {
     onTabClick(event, element) {
       event.preventDefault();
       let scrollTop = $(element.attr('href')).offset().top - this.tabContainerHeight + 1;
-      $('html, body').animate({ scrollTop: scrollTop }, 600);
+      $('html, body').animate({ scrollTop: scrollTop }, 600, () => {
+        this.checkTabContainerPosition();
+      });
     }
     
   /**
